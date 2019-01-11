35d4abcc40b90a5ce1e5ec27a713d1a443c15826
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-01-11 14:40:32 -0500

    Basic implementation of ulid generate function

--- ulid.py
+++ ulid.py
@@ -5,7 +5,7 @@ import sys
 import logging
 
 # Number of bits each ulid component should have
-TIMESTAMP = 48
+TIMESTAMP = 50
 RANDOMNESS = 80
 
 # 32 Symbol notation
@@ -15,26 +15,34 @@ CROCKFORDS_BASE32 = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
 def generate():
     import time
 
-    # Get the Unix Time in milliseconds as an int
-    epoch = int(time.time() * 1000)
-    epoch_bits = format(epoch, f"0{TIMESTAMP}b")
-    logging.info(f"EPOCH BITS  {epoch_bits}")
-    rand_num = 0
+    epoch_bits = format(int(time.time() * 1000), f"0{TIMESTAMP}b")
+    # logging.info(len(epoch_bits))
+    # logging.info(f"EPOCH BITS  {epoch_bits}")
     rand_num_bits = ""
 
+    # Get the randomness bits
     if sys.version_info > (3, 6, 0):
         logging.info("Can use the secrets module for generation")
         import secrets
 
-        rand_num = secrets.randbits(80)
-        rand_num_bits = format(rand_num, f"0{RANDOMNESS}b")
-        logging.info(f"RANDOM BITS {rand_num_bits}")
+        # secrets.randbits returns the random bits in the form of an int
+        rand_num_bits = bin(secrets.randbits(80))[2:]
+        # logging.info(f"RANDOM BITS {rand_num_bits}")
     else:
         logging.info("Use the os urandom method for generation")
 
+        rand_num_bits = bin(
+            int.from_bytes(os.urandom(RANDOMNESS / 8), byteorder="big")
+        )[2:]
+        # logging.info(f"RANDOM BITS {rand_num_bits}")
+
+    ulid_bits = epoch_bits + rand_num_bits
+    ulid_str = ""
+    for i in range(0, len(ulid_bits), 5):
+        ulid_str += CROCKFORDS_BASE32[int(ulid_bits[i : i + 5], base=2)]
+    return ulid_str
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    generate()
-
+    new_ulid = generate()
+    print(new_ulid)
