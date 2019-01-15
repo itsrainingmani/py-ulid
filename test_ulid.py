ca91a80a68cb532fb10a025e6eb264058bc6e451
Manikandan Sundararajan <mtiruman@buffalo.edu>
2019-01-14 22:00:29 -0500

    Test for min and max of ULID int value

--- test_ulid.py
+++ test_ulid.py
@@ -1,6 +1,7 @@
 # content of test_ulid.py
 
 import ulid
+import pytest
 
 
 class TestUlid(object):
@@ -12,3 +13,11 @@ class TestUlid(object):
     def test_ulid_object(self):
         _ulid = ulid.ULID(32143215987)
         assert _ulid != ulid.generate()
+
+    def test_ulid_max(self):
+        with pytest.raises(ValueError, match=r".*128-bit.*"):
+            ulid.ULID(340282366920938463463374607431768211459)
+
+    def test_ulid_min(self):
+        with pytest.raises(ValueError, match=r".*128-bit.*"):
+            ulid.ULID(-1)
