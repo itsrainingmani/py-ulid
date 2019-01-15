40c80df030e06ec52040e05b3a8fc8101c450043
Manikandan Sundararajan <mtiruman@buffalo.edu>
2019-01-14 20:55:51 -0500

    Move generate function outside class

--- ulid.py
+++ ulid.py
@@ -12,7 +12,7 @@ import sys
 __author__ = "Manikandan Sundararajan <tsmanikandan@protonmail.com>"
 
 # Number of bits each ulid component should have
-_timestamp = 50
+_time = 50
 _randomness = 80
 
 # 32 Symbol notation
@@ -30,6 +30,9 @@ class ULID:
     same millisecond)
     """
 
+    int = 0
+    bytes = bytes_(int)
+
     def __init__(self, bytes=None, int=None):
 
         if [bytes, int].count(None) != 1:
@@ -38,38 +41,37 @@ class ULID:
         if int is not None:
             if not 0 <= int < 1 << 128:
                 raise ValueError("int is out of range (need a 128-bit value")
+            self.int = int
+            self.bytes = self.int.to_bytes(8, byteorder="big")
 
         if bytes is not None:
             if len(bytes) != 16:
                 raise ValueError("bytes is not a 16-char string")
             assert isinstance(bytes, bytes_), repr(bytes)
-            int = int_.from_bytes(bytes)
-
-    # Function to generate the ulid
-    def generate(self):
-        import time
-
-        epoch_bits = format(int(time.time() * 1000), f"0{_timestamp}b")
-        # logging.info(f"EPOCH BITS  {epoch_bits}")
-
-        rand_num_bits = ""
-        try:
-            rand_bytes = os.urandom(_randomness // 8)
-        except NotImplementedError:
-            raise NotImplementedError(
-                "The /dev/urandom device is not available or readable"
-            )
-
-        # Get the randomness bits
-        rand_num_bits = bin(int.from_bytes(rand_bytes, byteorder="big"))[2:]
-
-        ulid_bits = epoch_bits + rand_num_bits
-        ulid_str = ""
-        for i in range(0, len(ulid_bits), 5):
-            ulid_str += _crockford_base[int(ulid_bits[i : i + 5], base=2)]
-        return ulid_str
+            self.int = int_.from_bytes(bytes)
+            self.bytes = bytes
 
 
-if __name__ == "__main__":
-    new_ulid = generate()
-    print(new_ulid)
+# Function to generate the ulid
+def generate():
+    import time
+
+    epoch_bits = format(int(time.time() * 1000), f"0{_time}b")
+    # logging.info(f"EPOCH BITS  {epoch_bits}")
+
+    rand_num_bits = ""
+    try:
+        rand_bytes = os.urandom(_randomness // 8)
+    except NotImplementedError:
+        raise NotImplementedError(
+            "The /dev/urandom device is not available or readable"
+        )
+
+    # Get the randomness bits
+    rand_num_bits = bin(int.from_bytes(rand_bytes, byteorder="big"))[2:]
+
+    ulid_bits = epoch_bits + rand_num_bits
+    ulid_str = ""
+    for i in range(0, len(ulid_bits), 5):
+        ulid_str += _crockford_base[int(ulid_bits[i : i + 5], base=2)]
+    return ulid_str
