77b3d6c91f8b6b0bd03ef8581976f1d7c5691578
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-05-30 21:32:47 -0400

    Create and flesh out an setup.py file

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The contents of the Readme file
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name="py-ulid",
+    version="1.0.0",
+    author="Manikandan Sundararajan",
+    author_email="me@tsmanikandan.com",
+    license="MIT",
+    description="Python library that provides an implementation of the Universally Unique Lexicographically Sortable Id spec",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tsmanikandan/py-ulid",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(exclude=("tests",)),
+    include_package_data=True,
+)
+
