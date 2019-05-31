9696022140918204a0caf57bab370f15e2e82b9b
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-05-30 22:28:48 -0400

    Move ulid to root of repository and update version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="py-ulid",
-    version="1.0.0",
+    version="1.0.1",
     description="Python library that provides an implementation of ULID Specification",
     long_description=README,
     long_description_content_type='text/markdown',
