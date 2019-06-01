2e4e89a00bcd2d7e6c881a54333dac09220565f9
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-06-01 02:56:13 -0400

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="py-ulid",
-    version="1.0.1",
+    version="1.0.2",
     description="Python library that provides an implementation of the ULID Specification",
     long_description=README,
     long_description_content_type='text/markdown',
